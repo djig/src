b5a339c5ce4f91066e2e870a9f2ffdad6b5bbf57
djig <jig_nash@yahoo.com>
2015-10-06 05:28:49 -0700

    Update app_Routes.js

--- js/app_Routes.js
+++ js/app_Routes.js
@@ -2,21 +2,28 @@
 ddApp.config(
     function ($routeProvider, $locationProvider,$provide ) {
         // var dsList = GblService.getGobals().dataSourcesName;
-
-
-
         $routeProvider
             .when('/Clients/:ClientCode/:asofdate', {
                 templateUrl: 'partials/_cPage.html',
                 controller: 'ClientCntrl'
             })
             .when('/Clients/:ClientCode/:asofdate/:entityId', {
-                templateUrl: 'partials/_cPage.html',
+                templateUrl: 'partials/_cAcPage.html',//account details
                 controller: 'ClientCntrl'
             })
-
+             .when('/Clients/:RMCSRCode/:asofdate/', {
+                templateUrl: 'partials/_cRMPage.html',
+                controller: 'ClientRMCSRCntrl'
+            })
+            .when('/Clients/:SecurityCode/:asofdate', {
+                templateUrl: 'partials/_cSecPage.html',
+                controller: 'ClientSecCntrl'
+            })
+            .otherwise({
+                    redirectTo: '/'// landing search page
+                });
             ;
-            console.log($locationProvider)
+          //  console.log($locationProvider)
 
         //  $locationProvider.html5Mode(true);
           // $provide.decorator('$sniffer', function($delegate) {
